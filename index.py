3f10eee6c76300224eb6bb2fec16a1f16868ed10
yubang <yubang93@gmail.com>
2016-04-20 11:29:25 +0800

    init

---
+++ index.py
@@ -0,0 +1,16 @@
+# coding:UTF-8
+
+
+"""
+前端模块化小工具，目标人群是0基础的前端和后台
+@author： yubang
+2016.04.20
+"""
+
+
+def test():
+    print("ok")
+
+
+if __name__ == '__main__':
+    test()
---
+++ lib/all_to_js.py
@@ -0,0 +1,17 @@
+# coding:UTF-8
+
+
+"""
+这个文件的目标是所有的数据都转换成js
+@author: yubang
+2016.04.20
+"""
+
+
+def build_js_data(source_str):
+    """
+    原始数据
+    :param source_str:
+    :return:
+    """
+    pass
